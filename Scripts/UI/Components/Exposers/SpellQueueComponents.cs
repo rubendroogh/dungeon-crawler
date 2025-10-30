022bb8e5bb29a055a881c1169b826035917e18e3
Ruben Droogh <rubendroogh@hotmail.nl>
2025-10-30 22:10:01 +0100

    Fixed not getting spell queue label correctly.

--- Scripts/UI/Components/Exposers/SpellQueueComponents.cs
+++ Scripts/UI/Components/Exposers/SpellQueueComponents.cs
@@ -3,7 +3,10 @@ public static partial class Components
     /// <summary>
     /// The SpellQueue rich text label containing the spell queue.
     /// </summary>
-    public const string SpellQueueLabel = "MarginContainer";
+    public const string SpellQueueLabel = "SpellQueueLabel";
 
+    /// <summary>
+    /// The root node for the spell queue debug components.
+    /// </summary>
     public const string SpellQueueRoot = "SpellQueue";
 }
