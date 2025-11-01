f5eee4a155eb141a300d550767c29f566d0695c2
Ruben Droogh <rubendroogh@hotmail.nl>
2025-11-01 13:56:33 +0100

    WIP ManaSourceManager rework.

--- Scripts/Managers/ManaSourceManager.cs
+++ Scripts/Managers/ManaSourceManager.cs
@@ -3,240 +3,178 @@ using System.Collections.Generic;
 using System.Linq;
 
 /// <summary>
-/// Represents a deck of blessings. These blessings are used to cast spells in the game.
-/// Initially, it contains only the Two of each domain.
-/// 
-/// TODO: Make this into a proper manager like the rest.
+/// The manager responsible for handling mana sources and their related functionalities.
+/// For now, this is only the blessing bars.
 /// </summary>
 public partial class ManaSourceManager : Node
 {
-	private List<Blessing> Cards { get; set; } = [];
+	// TODO: Add event for when mana sources change (e.g., blessings are added/removed).
 
-	private static Vector2I TileSize = new Vector2I(16, 16);
-
-	public override void _Ready()
-	{
-		base._Ready();
-		// InitializeCustomBlessingDeck();
-		// InitializeCardDeckUI();
-	}
+	public Dictionary<Domain, BlessingBar> BlessingBars { get; private set; } = new Dictionary<Domain, BlessingBar>();
 
 	/// <summary>
-	/// Gets the icon texture for a specific card based on its rank and suit.
+	/// Checks if the current mana sources can pay for the given spell cost.
 	/// </summary>
-	/// <returns>A texture representing the card's icon.</returns>
-	public static Texture2D GetCardIcon(Blessing card)
+	/// <param name="spellCost">The cost of the spell.</param>
+	/// <returns>True if the mana sources can pay for the spell, false otherwise.</returns>
+	public bool CanPay(SpellCost spellCost)
 	{
-		if (card == null)
+		// Aggregate all costs by domain
+		var costByDomain = new Dictionary<Domain, int>();
+		foreach (var cost in spellCost.Costs)
 		{
-			GD.PrintErr("Card is null");
-			return null;
-		}
-
-		Texture2D atlas = GD.Load<Texture2D>("res://Assets/RawImages/Cards.png");
-
-		// Create AtlasTexture for the card
-		AtlasTexture iconTexture = new AtlasTexture();
-		iconTexture.Atlas = atlas;
-
-		// Calculate tile position
-		int suitIndex = (int)card.Domain; // row
-		int rankIndex = (int)card.Rank - 1; // column (Ace = 1, so subtract 1)
-
-		Rect2 region = new Rect2(rankIndex * TileSize.X, suitIndex * TileSize.Y, TileSize.X, TileSize.Y);
-		iconTexture.Region = region;
-
-		return iconTexture;
-	}
-
-	/// <summary>
-	/// Adds a blessing to the deck and updates the UI.
-	/// </summary>
-	public void AddBlessing(Blessing blessing)
-	{
-		if (blessing == null)
-		{
-			GD.PrintErr("Card is null");
-			return;
-		}
-
-		Cards.Add(blessing);
-
-		// TODO: This can be a bit cleaner
-		ResetBlessingUI();
-		InitializeCardDeckUI();
-	}
-
-	/// <summary>
-	/// Initializes a full card deck with all 52 cards.
-	/// Probably not used for now, but useful for reference.
-	/// </summary>
-	private void InitializeFullCardDeck()
-	{
-		// Add all Zer
-		for (int i = 1; i < 14; i++)
-		{
-			Cards.Add(new Blessing
+			if (!costByDomain.ContainsKey(cost.Type))
 			{
-				Rank = (Rank)i,
-				Domain = Domain.Zer
-			});
+				costByDomain[cost.Type] = 0;
+			}
+
+			costByDomain[cost.Type] += cost.Amount;
 		}
 
-		// Add all Hamin
-		for (int i = 1; i < 14; i++)
+		// Check if we have enough mana in each domain
+		foreach (var domainCost in costByDomain)
 		{
-			Cards.Add(new Blessing
+			if (GetRemainingMana(domainCost.Key) < domainCost.Value)
 			{
-				Rank = (Rank)i,
-				Domain = Domain.Hamin
-			});
+				return false;
+			}
 		}
 
-		// Add all Calina
-		for (int i = 1; i < 14; i++)
-		{
-			Cards.Add(new Blessing
-			{
-				Rank = (Rank)i,
-				Domain = Domain.Calina
-			});
-		}
-
-		// Add all Jaddis
-		for (int i = 1; i < 14; i++)
-		{
-			Cards.Add(new Blessing
-			{
-				Rank = (Rank)i,
-				Domain = Domain.Jaddis
-			});
-		}
+		return true;
 	}
 
-	/// <summary>
-	/// Initializes a custom blessing deck with only the Two of each domain.
-	/// </summary>
-	private void InitializeCustomBlessingDeck()
+	private int GetRemainingMana(Domain domain)
 	{
-		var customDeck = new List<Blessing>
+		// Get the blessing bar for the domain
+		if (!BlessingBars.TryGetValue(domain, out var blessingBar))
 		{
-			new Blessing { Rank = Rank.Two, Domain = Domain.Calina },
-			new Blessing { Rank = Rank.Two, Domain = Domain.Hamin },
-			new Blessing { Rank = Rank.Two, Domain = Domain.Jaddis },
-			new Blessing { Rank = Rank.Two, Domain = Domain.Zer },
-		};
-		Cards = customDeck;
+			return 0;
+		}
+
+		// Get the available blessings and get the total mana from them by summing their levels
+		var availableBlessings = blessingBar.AvailableBlessings;
+		var count = availableBlessings.Sum(b => (int)b.Level);
+
+		return count;
 	}
 
-	/// <summary>
-	/// Initializes the UI for the card deck.
-	/// </summary>
-	private void InitializeCardDeckUI()
+	private void ShowOrHideBlessingBars()
 	{
-		string containersPath = "Margin/CardListHorizontalContainer/CardList/";
 
-		var containerHearts = GetNode<VBoxContainer>(containersPath + "CardListHearts");
-		foreach (Blessing card in Cards.Where(c => c.Domain == Domain.Calina))
-		{
-			TextureRect cardUI = CreateBlessingUI(card);
-			containerHearts.AddChild(cardUI);
-		}
-
-		var containerDiamonds = GetNode<VBoxContainer>(containersPath + "CardListDiamonds");
-		foreach (Blessing card in Cards.Where(c => c.Domain == Domain.Hamin))
-		{
-			TextureRect cardUI = CreateBlessingUI(card);
-			containerDiamonds.AddChild(cardUI);
-		}
-
-		var containerSpades = GetNode<VBoxContainer>(containersPath + "CardListSpades");
-		foreach (Blessing card in Cards.Where(c => c.Domain == Domain.Jaddis))
-		{
-			TextureRect cardUI = CreateBlessingUI(card);
-			containerSpades.AddChild(cardUI);
-		}
-
-		var containerClubs = GetNode<VBoxContainer>(containersPath + "CardListClubs");
-		foreach (Blessing card in Cards.Where(c => c.Domain == Domain.Zer))
-		{
-			TextureRect cardUI = CreateBlessingUI(card);
-			containerClubs.AddChild(cardUI);
-		}
-	}
-
-	/// <summary>
-	/// Resets the UI for the blessing list by clearing all displayed blessings.
-	/// </summary>
-	private void ResetBlessingUI()
-	{
-		string containersPath = "Margin/CardListHorizontalContainer/CardList/";
-
-		var containerHearts = GetNode<VBoxContainer>(containersPath + "CardListHearts");
-		foreach (var child in containerHearts.GetChildren())
-		{
-			child.QueueFree();
-		}
-
-		var containerDiamonds = GetNode<VBoxContainer>(containersPath + "CardListDiamonds");
-		foreach (var child in containerDiamonds.GetChildren())
-		{
-			child.QueueFree();
-		}
-
-		var containerSpades = GetNode<VBoxContainer>(containersPath + "CardListSpades");
-		foreach (var child in containerSpades.GetChildren())
-		{
-			child.QueueFree();
-		}
-
-		var containerClubs = GetNode<VBoxContainer>(containersPath + "CardListClubs");
-		foreach (var child in containerClubs.GetChildren())
-		{
-			child.QueueFree();
-		}
-	}
-
-	/// <summary>
-	/// Creates a TextureRect UI element for a blessing using an atlas texture.
-	/// </summary>
-	private TextureRect CreateBlessingUI(Blessing blessing)
-	{
-		var texture = GetCardIcon(blessing);
-		TextureRect blessingUI = new BlessingUI
-		{
-			Card = blessing,
-			Texture = texture,
-			CustomMinimumSize = TileSize * 2,
-			ExpandMode = TextureRect.ExpandModeEnum.KeepSize,
-			Scale = new Vector2(2, 2)
-		};
-
-		return blessingUI;
 	}
 }
 
 /// <summary>
-/// Represents a blessing card with a rank and domain (which is its type).
-/// This is used to cast spells in the game.
+/// Represents a blessing bar that tracks the current and maximum mana for a specific blessing.
+/// </summary>
+public class BlessingBar
+{
+	/// <summary>
+	/// The domain of the blessing bar.
+	/// Each blessing bar corresponds to a specific domain.
+	/// </summary>
+	public Domain Domain { get; set; }
+
+	/// <summary>
+	/// All blessings that have been added to the blessing bar, including spent ones.
+	/// </summary>
+	public List<Blessing> AllBlessings { get; set; } = new List<Blessing>();
+
+	/// <summary>
+	/// Blessings that are marked for use in a queued action.
+	/// TODO: Find a way to track which blessings are used by which action so they can be properly managed.
+	/// </summary>
+	public List<Blessing> BlessingsMarkedForUse => AllBlessings.Where(b => b.State == BlessingState.MarkedForUse).ToList();
+
+	/// <summary>
+	/// The current amount of unspent mana in the blessing bar.
+	/// </summary>
+	public List<Blessing> AvailableBlessings => AllBlessings.Where(b => b.State == BlessingState.Available).ToList();
+
+	/// <summary>
+	/// The maximum amount of mana the blessing bar can hold.
+	/// </summary>
+	public int MaxMana { get; set; }
+
+	/// <summary>
+	/// Checks if the blessing bar can pay for the given mana cost.
+	/// </summary>
+	public bool CanPay(ManaCost cost)
+	{
+		int totalAvailable = AvailableBlessings.Count(b => b.Domain == cost.Type);
+		return totalAvailable >= cost.Amount;
+	}
+
+	/// <summary>
+	/// Adds a blessing to the blessing bar if there is enough space.
+	/// </summary>
+	public void AddBlessing(Blessing blessing)
+	{
+		if (blessing.Domain != Domain)
+		{
+			GD.PrintErr("Cannot add blessing of domain " + blessing.Domain + " to blessing bar of domain " + Domain);
+			return;
+		}
+
+		// Check if there is enough space
+		if (AvailableBlessings.Count + (int)blessing.Level < MaxMana)
+		{
+			AvailableBlessings.Add(blessing);
+		}
+	}
+
+	public 
+}
+
+/// <summary>
+/// Represents a blessing with a rank and domain (which is its type).
+/// This is used to cast spells in the game and fills the blessing bar.
 /// </summary>
 public class Blessing
 {
-	public Rank Rank { get; set; }
+	/// <summary>
+	/// The potency level of the blessing. This determines how much mana it provides.
+	/// </summary>
+	public Rank Level { get; set; }
 
+	/// <summary>
+	/// The current state of the blessing (Available, MarkedForUse, Spent).
+	/// </summary>
+	public BlessingState State { get; set; } = BlessingState.Available;
+
+	/// <summary>
+	/// The domain of the blessing.
+	/// TODO: Maybe remove this and track the domain via the blessing bar instead?
+	/// </summary>
 	public Domain Domain { get; set; }
-
-    public Texture2D GetIcon()
-	{
-		return ManaSourceManager.GetCardIcon(this);
-	}
 }
 
 /// <summary>
 /// Represents the rank of a card in a standard deck.
-/// The ranks are ordered from Two (1) to Ace (14).
+/// The ranks are ordered from Minor (1) to Major (4).
 /// </summary>
-public enum Rank { Two = 1, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace }
+public enum Rank { Minor = 1, Lesser = 2, Greater = 3, Major = 4, Superior = 5 }
+
+/// <summary>
+/// Represents the state of a blessing in the game.
+/// </summary>
+public enum BlessingState
+{
+	/// <summary>
+	/// The blessing is available for use.
+	/// </summary>
+	Available,
+
+	/// <summary>
+	/// The blessing is marked for use in a queued action.
+	/// </summary>
+	MarkedForUse,
+
+	/// <summary>
+	/// The blessing has been spent (used) and cannot be used again until refreshed by prayer.
+	/// </summary>
+	Spent
+}
 
 /// <summary>
 /// Represents the type of a blessing in a standard deck.
@@ -247,7 +185,7 @@ public enum Domain
 	/// Focused on power and resilience.
 	/// </summary>
 	Calina,
-	
+
 	/// <summary>
 	/// Focused on wisdom and calm.
 	/// </summary>
